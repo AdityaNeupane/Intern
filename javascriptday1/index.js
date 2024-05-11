4049dec76a636d1195ff16cc0a31fcc0a2f4c35b
adityaneupane <bijayneupane324@gmail.com>
2024-05-11 21:57:50 +0545

    javascript beginning

---
+++ javascriptday1/index.js
@@ -0,0 +1,29 @@
+// var numberone= 10;
+// var numbertwo= 20;
+
+
+//  var sum= numberone+numbertwo;
+//  var multiply=numberone*numbertwo;
+//  var substract=numberone-numbertwo;
+//  var divide=numberone/numbertwo;
+
+// console.log( sum);
+// console.log(multiply);
+// console.log(substract);
+// console.log(divide);
+
+// ......
+
+// var a=sanjok;
+// var b=karki;
+// var c=chitwan;
+// var d=20;
+
+// console.log(` my name is {$a} +  {$b} . i live in + {$c} . i am + {$d} years old.`);
+ 
+// .........
+
+function hello()
+{
+    document.getElementById("p2").innerHTML ="can do anything";
+}
