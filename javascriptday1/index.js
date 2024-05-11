67e3addce03f14cde95b1d93d20237fd5ea80f5c
adityaneupane <bijayneupane324@gmail.com>
2024-05-11 23:03:33 +0545

    new changes

--- javascriptday1/index.js
+++ javascriptday1/index.js
@@ -1,25 +1,25 @@
-// var numberone= 10;
-// var numbertwo= 20;
+var numberone= 10;
+var numbertwo= 20;
 
 
-//  var sum= numberone+numbertwo;
-//  var multiply=numberone*numbertwo;
-//  var substract=numberone-numbertwo;
-//  var divide=numberone/numbertwo;
+ var sum= numberone+numbertwo;
+ var multiply=numberone*numbertwo;
+ var substract=numberone-numbertwo;
+ var divide=numberone/numbertwo;
 
-// console.log( sum);
-// console.log(multiply);
-// console.log(substract);
-// console.log(divide);
+console.log( sum);
+console.log(multiply);
+console.log(substract);
+console.log(divide);
 
 // ......
 
-// var a=sanjok;
-// var b=karki;
-// var c=chitwan;
-// var d=20;
+var a="sanjok";
+var b="karki";
+var c="chitwan";
+var d=20;
 
-// console.log(` my name is {$a} +  {$b} . i live in + {$c} . i am + {$d} years old.`);
+console.log(` my name is ${a} ${b}. i live in ${c}. i am ${d} years old.`);
  
 // .........
 
@@ -27,3 +27,61 @@ function hello()
 {
     document.getElementById("p2").innerHTML ="can do anything";
 }
+
+
+// ..........
+
+// console.log(name);
+{
+    var name = "aditya";
+    name = "ashok";
+}
+console.log(name);
+
+// .......
+
+var one="20";
+var two="40";
+
+console.log(one*two);
+
+// .....
+
+var a="aditya";
+var b="neupane";
+
+console.log(`${a} ${b}`);
+
+// .........
+
+var a = "aditya";
+var b = "neupane";
+var c = "bharatpur";
+var d = 22;
+
+console.log(`hello, my name is ${a} ${b}. i live in ${c}. i am ${d} years old.`);
+
+// .....
+
+const person = 
+{
+    firstname: "brian",
+    lastname: "lara",
+    age: 45,
+    haircolor: "blue",
+};
+
+console.table(person);
+
+
+// .........
+
+var people = 
+{
+    firstname: "aditya",
+    lastname: "neupane",
+    age: 22,
+    address: "bharatpur-9",
+};
+console.table(people);
+
