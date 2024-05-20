20f94b5d57c3b4e9a46fdc3fa168c8f52fe99c2a
adityaneupane <bijayneupane324@gmail.com>
2024-05-20 20:36:00 +0545

    new changes

--- jsday8/task.js
+++ jsday8/task.js
@@ -23,13 +23,13 @@ fetch("https://dummyjson.com/recipes")
          <p><strong>Instructions:</strong> ${recipe.instructions.join(' , ')}</p>
          <p><strong>PrepTimeMinutes:</strong> ${recipe.prepTimeMinutes} miunutes</p>
          <p><strong>CookTimeMinutes:</strong> ${recipe.cookTimeMinutes} minutes</p>
-         <p><strong>Servings:</strong> ${recipe.servings}</p>
+         <p><strong>Servings:</strong> ${recipe.servings} servings</p>
          <p><strong>Difficulty:</strong> ${recipe.difficulty}</p>
          <p><strong>Cuisine:</strong> ${recipe.cuisine}</p>
-         <p><strong>CaloriesPerServings:</strong> ${recipe.caloriesPerServing}</p>
+         <p><strong>CaloriesPerServings:</strong> ${recipe.caloriesPerServing} calories</p>
          <p><strong>Tags:</strong> ${recipe.tags.join(' , ')}</p>
          <p><strong>UserID:</strong> ${recipe.userId}</p>
-         <p><strong>Rating:</strong> ${recipe.rating}</p>
+         <p><strong>Rating:</strong> ${recipe.rating} out of 5</p>
          <p><strong>ReviewCount:</strong> ${recipe.reviewCount}</p>
          <p><strong>MealType:</strong> ${recipe.mealType}</p>
           
